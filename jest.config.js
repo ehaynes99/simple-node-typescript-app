952b276d3fd95e6523c1a05414eab902b5d15d63
Eric Haynes <ehaynes99@gmail.com>
2022-07-30 20:13:31 -0400

    switch ts-jest for @swc/jest

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,10 @@
 module.exports = {
-  preset: 'ts-jest',
   testEnvironment: 'node',
-  transform: { '^.+\\.[jt]s$': 'ts-jest' },
+  transform: { '^.+\\.(t|j)sx?$': ['@swc/jest'] },
   testMatch: ['<rootDir>/src/**/*.(spec|test).(t|j)s'],
   clearMocks: true,
   collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/src/**/*'],
   coverageReporters: ['text', 'lcov', 'cobertura'],
   coverageDirectory: './coverage',
-  coveragePathIgnorePatterns: ['index.ts'],
 };
