ec18f4a673596e88aee8db0b14cc20efe0e7aabb
Eric Haynes <eric.haynes@openphone.co>
2022-10-13 17:24:32 -0400

    Fix script to allow different paths

--- create-project.sh
+++ create-project.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
+
 if [ "$#" -ne 1 ]
 then
-  echo "Must supply a name"
-  exit 1
+ echo "Must supply a name"
+ exit 1
 fi
 
-NAME="$1"
+FULL_PATH="$1"
+basename path/to/project
+DIR=$(dirname $FULL_PATH)
+NAME=$(basename $FULL_PATH)
+
+mkdir -p $DIR
+cd $DIR
 git clone git@github.com:ehaynes99/simple-node-typescript-app.git $NAME
 cd $NAME
 find . -type f -exec perl -pi -e"s/simple-node-typescript-app/${NAME}/g" {} +
@@ -17,4 +24,10 @@ rm create-project.sh
 git init .
 git add .
 git commit -m"Create project"
-code .
+
+read -p "Open with code? [Y]/n" -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Nn]$ ]]
+then
+  code .
+fi
